c34f6b0e2f99cc09981a72954124d1044398e3be
reefreefreef <reef_feer@proton.me>
2026-06-04 14:49:46 +0100

    Revert "test selector"
    This reverts commit e6e948472e56ce7ded6b4bc74a013bcf82a50375.

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -2,35 +2,28 @@ const db = require("./db/db.js")
 
 
 function weightedPick(picks) {
-    const totalWeight = picks.reduce((partialSum, a) => partialSum + a.weight, 0)
-    console.log(totalWeight, picks)
-   
-    var i, sum=0, r=Math.random();
-    for (i in picks) {
-        sum += picks[i].weight;
-        if (r <= sum) return i;
-    }
-
+    console.log(picks)
 }
 
 
-async function getChallengeWeights(count) {
-    var counts = await db.raw('select challenges.id, challenges.title, counts.c, challenges.last_shown from challenges left join (select challengeId, count(*) as c from guesses group by challengeId) as counts on counts.challengeId=challenges.id')
+async function getLeastGuessed(count) {
+    var counts = await db.raw('select challenges.id, challenges.title, counts.c from challenges left join (select challengeId, count(*) as c from guesses group by challengeId) as counts on counts.challengeId=challenges.id')
 
-    for (let i = 0; i < counts.length; i++) {
-        const challenge = counts[i];
-
-        challenge.weight = 
-            (challenge.c/5) +
-            ((new Date()).getTime()-(challenge.last_shown||-Infinity))/(1000*60*60*6)    
-            
-        console.log(challenge.title, (challenge.c/5), ((new Date()).getTime()-challenge.last_shown)/(1000*60*60*6)    )
-        
-    }
+    counts = counts.sort((a,b)=>{
+        return a.c-b.c
+    })
     
     return counts.slice(0, Math.min(counts.length, count))
 }
+async function getLeastRecent(count) {
+    var counts = await db.raw('select id, title, last_shown from challenges')
 
+    counts = counts.sort((a,b)=>{
+        return a.last_shown-b.last_shown
+    })
+    
+    return counts.slice(0, Math.min(counts.length, count))
+}
 
 async function selectChallenge(id) {
     await db("app_flags").where("key", "current_challenge").update("value", chosen.id)
@@ -39,9 +32,7 @@ async function selectChallenge(id) {
 }
 
 async function test() {
-    const weights = getChallengeWeights(50)
-    console.log(weights)
-    console.log(weightedPick(weights))
+    console.log(await getLeastRecent(20))
 }
 
 
